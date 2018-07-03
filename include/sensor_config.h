877e3d9c14eab9fc58d3024bb4444176d0612538
Patric Kanngießer <mail@lpnw.de>
2018-07-03 21:33:46 +0200

    Removed comment

--- include/sensor_config.h
+++ include/sensor_config.h
@@ -1,11 +1,7 @@
-//
-// Created by patric on 02.07.2018.
-//
-
 #ifndef ESPTEMPSENSOR_SENSOR_CONFIG_H
 #define ESPTEMPSENSOR_SENSOR_CONFIG_H
-    #define DEVICE_NAME "TestSensor"
-    #define INTERVAL_SEK 30
+    #define DEVICE_NAME ""
+    #define INTERVAL_SEK 120
     #define WIFI_SSID ""
     #define WIFI_PWD ""
 
@@ -13,10 +9,9 @@
     #define MQTT_PORT 8883
     #define MQTT_USER ""
     #define MQTT_PWD ""
-    #define MQTT_BASE_CHANNEL "home/sensors/testsensor"
+    #define MQTT_BASE_CHANNEL "home/sensors/test"
 
     #ifdef ENABLE_SSL
-
         #define SSL_SHA1_FINGERPRINT {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00}
     #endif
 #endif //ESPTEMPSENSOR_SENSOR_CONFIG_H
